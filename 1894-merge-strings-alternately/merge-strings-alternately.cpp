e399275807b8bf8bd18669b25e2620268111859c
notVSK <vaisakh.s.kumar5476980@gmail.com>
2025-03-16 16:39:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (63.28%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+       int word = 1;//for switching the word alterantively
+       int A = word1.size();//for traversing the 1st word 
+       int B = word2.size();//for traversing the 2nd word
+       int a = 0; int b = 0;//pointer for word1 and word2
+       string result;// for printing result
+
+       while(a < A && b < B){//while the pointer is not in the end of the word
+        result += word1[a];
+        result += word2[b];
+        a++; b++;
+       }
+       while(a < A){// complete the remaining word if left
+        result += word1[a];
+        a++;
+       }
+       while(b < B){// complete the remaining word if left
+        result += word2[b];
+        b++;
+       }
+    return result;
+    }
+};
